3ae9f0d7c05c2ccfa4f30406a8298898db07af0d
Mayank Goel <f2014768@pilani.bits-pilan.ac.in>
2015-08-03 23:02:48 +0530

    Notifications and Sponsors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Background transition behind sidenav
+
 $('.navback').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).parent().find('a').attr('href') ).offset().top
@@ -13,36 +15,53 @@ $(function(){
     return true;
 });
 
-/*
-$(function(){
-    $('#navbar').h(function(){
-        var tp = $('body').scrollTop();
-        $('#checkpos').text(tp);    
-        $(this).animate({"margin-left":'0px'},500);
-    },function(){      
-        $(this).animate({"margin-left":'-10%'},500);
-    });
-});
-*/
+//menu-button on-click working
 
-$(document).ready(function(){
-    var count = 0;
-    $("#navbar").click(function(){
-        count++;
-    //even odd click detect 
-            var isEven = function(someNumber) {
-            return (someNumber % 2 === 0) ? true : false;
-            };
-    // on odd clicks do this
-    if (isEven(count) === false) {
-        $("#navbar").animate({"margin-left":'-10%'},500);
-    }
-    // on even clicks do this
-    else if (isEven(count) === true) {
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function(event) {
+    event.preventDefault();
+    this.classList.toggle( "active" );
+
+    var tp = $("#nav-toggle").hasClass('active') ;
+     
+
+    if( tp === true)
+    {
         $("#navbar").animate({"margin-left":'0px'},500);
-            }
-    });
-});
+    }
+
+    else if(tp === false)
+    {
+        var wid= $('#navbar').width();
+        wid = (-wid) + 'px';
+        $("#navbar").animate({"margin-left":wid},500);
+    }
+
+  });
+
+//notifications on-click working
+
+document.querySelector( "#notify-toggle" )
+  .addEventListener( "click", function(event) {
+    event.preventDefault();
+    this.classList.toggle( "active" );
+
+    var tp = $("#notify-toggle").hasClass('active') ;
+     
+
+    if( tp === true)
+    {
+        $("#notify-bar").animate({"margin-right":'0px'},500);
+    }
+
+    else if(tp === false)
+    {
+        var wid= $('#notify-bar').width();
+        wid = (-wid) + 'px';
+        $("#notify-bar").animate({"margin-right":wid},500);
+    }
+
+  }); 
 
 $("#click").click(function() {
   $("#cover").removeClass('hidden');
@@ -53,4 +72,61 @@ $("#x, #cover").click(function() {
   $("#cover").addClass('hidden');
   $("#message").addClass('hidden');
   $("#click").removeClass('hidden');
+});
+
+//menu hiding function on scroll
+
+var flagscroll = 0;
+$(document).ready(function(){
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        
+         var  ht = $('body').height();
+         ht = parseInt(ht * 0.38) + 5;
+
+         $('#checkpos').text(ht);
+        if ($(this).scrollTop() > ht) {
+            if(flagscroll==0)
+            {
+                var wid= $('#navbar').width();
+                wid = (-wid) + 'px';
+                flagscroll = 1;
+                $("#navbar").animate({"margin-left":wid},300);
+                $("#notify-bar").animate({"margin-right":wid},300);
+                $('#nav-toggle').removeAttr('class');
+                $('#notify-toggle').removeAttr('class');
+                $('#mainnav').animate({'width':'100%'},700);
+                $('#notify-btn').css('border-left',' 1px solid  rgba(255,255,255,0.5)');
+                $('#login').css('border-bottom-right-radius','0px');
+                $('#mainnav').css('border-bottom-right-radius','0px');
+                $('#logo').css({'position':'fixed','top':'5px'});
+                $('#logo').animate({'height':'70px'},500,"linear");
+
+            }    
+        } 
+
+        else {
+            if(flagscroll==1)
+            {
+
+                flagscroll=0;
+                $("#navbar").animate({"margin-left":'0px'},300);
+                $("#notify-bar").animate({"margin-right":'0px'},300);
+                $('#nav-toggle').attr('class', 'active');
+                $('#notify-toggle').attr('class', 'active');
+                $('#mainnav').animate({'width':'195px'},700);
+                $('#notify-btn').css('border-left',' 0px solid  rgba(255,255,255,0.5)');
+                $('#login').css('border-bottom-right-radius','5px');
+                $('#mainnav').css('border-bottom-right-radius','5px');
+                $('#logo').css({'position':'absolute','top':'38vh'});
+                $('#logo').animate({'height':'22vh'},500,"linear");
+            }
+        }
+    });   
+});
+
+$('#login,#menubutton,#notify-btn').hover(function(){
+    $(this).css('background','rgba(139,141,182,0.7)');
+},function(){
+     $(this).css('background','transparent');
 });
